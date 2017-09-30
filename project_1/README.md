aaca82a3580dd8be1cf8dea5e63a5f88fe8a538a
vipulgpt10 <vipul.gupta@colorado.edu>
2017-09-30 04:08:01 -0600

    design stubs for memory.h

--- project_1/README.md
+++ project_1/README.md
@@ -1,9 +1,11 @@
-
+**************************************************************************
 #project1 repository
-
+**************************************************************************
 
 Author: Vipul Gupta
+
 Project: Project 1 - Build Systems and C Prohgramming Practice
+
 Members: 1. Vipul Gupta (vipul.gupta@colorado.edu)
 	 2. Sharanjeet Mago (sharanjeet.mago@colorado.edu)
 
@@ -12,3 +14,4 @@ and conversions on various platforms. It also includes the Makefile for
 native and cross compilation on platforms such as HOST (Linux VM), 
 Beagle Bone Black and Freedom KL25Z board.
 
+**************************************************************************
---
+++ project_1/src/memory.c
@@ -0,0 +1,50 @@
+/**
+ * @file memory.c
+ * @brief The implementation file for memory manupulation functions
+ *
+ * This implementation file provides the function definitions for
+ * various memory manupulation operations.
+ *
+ * @author Vipul Gupta
+ * @date 29 September 2017
+ *
+ */
+
+#include <stdio.h>
+#include "memory.h"
+
+
+uint8_t * my_memmove(uint8_t * src, uint8_t * dst, size_t length)
+{
+	//
+}
+
+uint8_t * my_memcpy(uint8_t * src, uint8_t * dst, size_t length)
+{
+	//
+}
+
+uint8_t * my_memset(uint8_t * src, size_t length, uint8_t value)
+{
+	//
+}
+
+uint8_t * my_memzero(uint8_t * src, size_t length)
+{
+	//
+}
+
+uint8_t * my_reverse(uint8_t * src, size_t length)
+{
+	//
+}
+
+int32_t * reserve_words(size_t length)
+{
+	//
+}
+
+void free_words(int32_t * src)
+{
+	//
+}
