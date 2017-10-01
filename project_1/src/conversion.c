1396934bab626633694627a6e104b77ff4d31acb
sharanjeetsinghmago <sharanjeetsinghmago@gmail.com>
2017-10-01 17:18:25 -0600

    design stubs for conversion.h

---
+++ project_1/src/conversion.c
@@ -0,0 +1,34 @@
+/**
+* @file conversion.c
+* @brief The implementation file for data manipulation functions
+*
+* This implementation file provides the function definitions for
+* various data manipulation operations.
+*
+* @author Sharanjeet Singh Mago
+* @date 30 September 2017
+*
+*/
+
+#include <stdio.h>
+#include "conversion.h"
+
+uint8_t my_itoa(int32_t data, uint8_t * ptr, uint32_t base);
+{
+ //
+}
+
+int32_t my_atoi(uint8_t * ptr, uint8_t digits, uint32_t base);
+{
+ //
+}
+
+int8_t big_to_little32(uint32_t * data, uint32_t length);
+{
+ //
+}
+
+int8_t little_to_big32(uint32_t * data, uint32_t length);
+{
+ //
+}
