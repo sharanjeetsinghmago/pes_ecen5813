9bb6c47feeef48117b089736d7872651f492b5bd
vipulgpt10 <vipul.gupta@colorado.edu>
2017-10-02 14:47:51 -0600

    adding main.c

---
+++ project_1/src/main.c
@@ -0,0 +1,21 @@
+/**
+ * @file main.c
+ * @brief The main file for executing project deliverables.
+ *
+ * This main file provides the function calls for project 1.
+ *
+ * @author Vipul Gupta
+ * @date 1 October 2017
+ *
+ */
+
+#include "project1.h"
+
+int main(void)
+{
+
+	#ifdef PROJECT1
+		project1();
+	#endif
+
+}
