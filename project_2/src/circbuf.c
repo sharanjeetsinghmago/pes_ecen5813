dad87e82c2ef49618e45a74227950fd398ef97b5
vipulgpt10 <vipul.gupta@colorado.edu>
2017-10-17 23:59:25 -0600

    Design stub for circular buffer files

---
+++ project_2/src/circbuf.c
@@ -0,0 +1,50 @@
+/**
+ * @file circbuf.c
+ * @brief The implementation file for circular buffer data structure
+ *
+ * This implementation file provides the function definitions for
+ * implememting and using circular buffer.
+ *
+ * @author Vipul Gupta
+ * @date 17 October 2017
+ *
+ */
+
+
+#include "circbuf.h"
+
+
+CB_status CB_buffer_add_item(CB_t * buff, uint8_t data)
+{
+	//
+}
+
+CB_status CB_buffer_remove_item(CB_t * buff, uint8_t data)
+{
+	//
+}
+
+CB_status CB_is_full(CB_t * buff)
+{
+	//
+}
+
+CB_status CB_is_empty(CB_t * buff)
+{
+	//
+}
+
+CB_status CB_peek(CB_t * buff, size_t position, uint8_t * data)
+{
+	//
+}
+
+CB_status CB_init(CB_t * buff, size_t length)
+{
+	//
+}
+
+CB_status CB_destroy(CB_t * buff)
+{
+	//
+}
